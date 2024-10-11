f55bcf23a8a604390487f2d085ec5652ff65effd
Maalshekto <tmartin@live.fr>
2024-10-11 21:26:51 +0200

    delete

--- main.tf
+++ main.tf
@@ -22,7 +22,8 @@ terraform {
 
 # Récupération des noms des dossiers dans le répertoire container-apps
 locals {
-  app_dirs = [for dir in fileset("${path.module}/container-apps", "*") : basename(dir)]
+  app_dirs = ["nodejs"]
+  # app_dirs = [for dir in fileset("${path.module}/container-apps", "*") : basename(dir)]
 }
 
 # Création d'un dépôt ECR pour chaque dossier
