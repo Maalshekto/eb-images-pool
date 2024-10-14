1abf1d64c1d0476b9479a27a469c3da6742ebbb8
Maalshekto <tmartin@live.fr>
2024-10-14 10:54:01 +0200

    NODEJS V1

--- container-apps/nodejs/app.js
+++ container-apps/nodejs/app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World V9 !!!\n');
+  res.end('Hello, World V1 !!!\n');
 });
 
 server.listen(port, hostname, () => {
